692a0239ccbe3458b92c2e39a2fa52a91921a68e
DESKTOP-DP3RJ5I\hi <sunnyskylove22@gmail.com>
2024-04-16 15:57:54 +0900

    [commit]

---
+++ 01_lecture-source/chap02-thymeleaf-lecture-source/src/main/java/com/ohgiraffers/thymeleaf/config/Chap02ThymeleafLectureSourceApplication.java
@@ -0,0 +1,15 @@
+package com.ohgiraffers.thymeleaf.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap02ThymeleafLectureSourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chap02ThymeleafLectureSourceApplication.class, args);
+	}
+
+}
+
+// 원래 있었던 것, config 패키지 만들어줘서 그 안에 넣는다~!!
